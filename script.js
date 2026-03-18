fe29fa3369837d5db92aed78c45995c8342cd579
shresile <46599124+shresile@users.noreply.github.com>
2026-03-18 22:35:01 +0800

    Fix a bug of not supporting consecutive operations
    1, Adjust the logic of function `operate`, it now returns
        a value, not sets display screen directly and not
        clears variables. Clearing variables is performed in
        event listener.
    2, Make code cleaner, adding new array `fourOperator` Makes
        it more readable. Simplify the process of assigning `op`.
    3, Add one more `if` condition in `operatorButtons` event listener,
        for consecutive operations like 1+15-3. In this situation, when
        `-` is pressed, the screen shows `16`, num1 = 16, op = 'subtract'.

--- script.js
+++ script.js
@@ -15,13 +15,13 @@ function divide (a,b) {
 }
 
 function operate (num1, op, num2) {
-    if (op == "add") displayWindow.textContent = add(num1, num2);
-    if (op == "subtract") displayWindow.textContent = subtract(num1, num2);
-    if (op == "multiply") displayWindow.textContent = multiply(num1, num2);
-    if (op == "divide") displayWindow.textContent = divide(num1, num2);
-    num1 = '';
-    num2 = '';
-    numTempt = '';
+    let result;
+    if (op == "add") result = add(num1, num2);
+    if (op == "subtract") result = subtract(num1, num2);
+    if (op == "multiply") result = multiply(num1, num2);
+    if (op == "divide") result = divide(num1, num2);
+    return result;
+   
 
 
 }
@@ -30,6 +30,7 @@ let num1;
 let num2;
 let op;
 let numTempt='';
+let result;
 
 //update numTemp when digit buttons are clicked
 //update display screen
@@ -37,32 +38,48 @@ let displayWindow = document.querySelector('.display');
 const digitButtons = document.querySelectorAll(".digit");
 digitButtons.forEach(button => button.addEventListener("click", (e) => {
     numTempt += e.target.textContent.toString();
-    console.log(numTempt);
+    console.log(numTempt);//can be deleted later
     displayWindow.textContent = numTempt;
 }));
 
 //when operator buttons are pressed
+let fourOperators = ["add", "subtract", "multiply", "divide"];
 const operatorButtons = document.querySelectorAll('.operator');
 operatorButtons.forEach(button => button.addEventListener("click", (e) => {
     
     let clickedButton = e.target.id;
-    console.log(`clicked button: ${clickedButton}`);
-    if (clickedButton == "add" || 
-        clickedButton =="subtract" || 
-        clickedButton =="multiply" || 
-        clickedButton == "divide") {
-        displayWindow.textContent = '';
-        num1 = +numTempt;      
-        numTempt = '';
-        if (clickedButton == "add" ) op = "add";
-        if (clickedButton == "subtract" ) op = "subtract";
-        if (clickedButton == "multiply" ) op = "multiply";
-        if (clickedButton == "divide" ) op = "divide";
-        console.log(`op: ${op}`);
+    console.log(`clicked button: ${clickedButton}`);//can be deleted later
+    if (fourOperators.includes(clickedButton)) {
+         if (op && num1) {
+            num2 = +numTempt;
+            console.log(`num1:${num1}, op: ${op}, num2: ${num2}`);//can be deleted later
+            result = operate (num1, op, num2); 
+            displayWindow.textContent = result;
+            num1 = result;
+            op = clickedButton;
+            num2 = '';
+            numTempt = '';              
+            result = '';
+
+         } else {
+            displayWindow.textContent = '';
+            num1 = +numTempt;      
+            numTempt = '';
+            op = clickedButton;
+            console.log(`op: ${op}`);//can be deleted later
+        }
+        
+        
     } else {        
         num2 = +numTempt;
-        console.log(`num1:${num1}, op: ${op}, num2: ${num2}`);
-        operate (num1, op, num2); 
+        console.log(`num1:${num1}, op: ${op}, num2: ${num2}`);//can be deleted later
+        result = operate (num1, op, num2); 
+        displayWindow.textContent = result;
+        num1 = '';
+        num2 = '';
+        numTempt = '';
+        op = '';    
+        result = '';
         
     }
 
